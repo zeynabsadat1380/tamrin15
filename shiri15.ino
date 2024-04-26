f806973d0a5e1437681fd57b10b81ade3faf4934
zeynabsadat1380 <167699936+zeynabsadat1380@users.noreply.github.com>
2024-04-26 14:03:55 +0330

    Add files via upload

---
+++ shiri15.ino
@@ -0,0 +1,19 @@
+#define BUZZER_PIN 11
+#define BTN_PIN 7
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(BTN_PIN))
+  {
+    analogWrite(BUZZER_PIN, 255);
+  }
+  else
+  {
+    analogWrite(BUZZER_PIN, 127);
+  }
+}
